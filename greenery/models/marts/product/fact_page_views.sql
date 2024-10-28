7562e66cc7207800862c994d7cc7ffe40b5e5e0c
Inah Canlapan <57849289+inahpatrizia@users.noreply.github.com>
2024-10-28 20:53:26 +0000

    added tests

--- greenery/models/marts/product/fact_page_views.sql
+++ greenery/models/marts/product/fact_page_views.sql
@@ -13,18 +13,20 @@ session_timing_agg as (
     select * from {{ ref('int_session_timings') }}
 )
 
-select  e.session_id,
+{% set event_types = dbt_utils.get_column_values(
+        table = ref('stg_events'),
+        column = 'event_type'
+    )
+%}
+
+select  distinct e.session_id,
         e.user_id,
-        coalesce(e.product_id, oi.product_id) as product_id,
         session_started_at,
         session_ended_at,
-        sum(case when e.event_type = 'page_view' then 1 else 0 end) as page_view,
-        sum(case when e.event_type = 'add_to_cart' then 1 else 0 end) as add_to_cart,
-        sum(case when e.event_type = 'checkout' then 1 else 0 end) as checkout,
-        sum(case when e.event_type = 'package_shipped' then 1 else 0 end) as packages_shipped,
+        {% for event_type in event_types %}
+        {{ sum_of('e.event_type', event_type) }} as {{ event_type }}s,
+        {%endfor%}
         datediff('minute', session_started_at, session_ended_at) as session_length_minutes
 from events e 
-left join order_items oi on e.order_id = oi.order_id
 left join session_timing_agg s on s.session_id = e.session_id
-group by 1,2,3,4,5
-
+group by 1,2,3,4
